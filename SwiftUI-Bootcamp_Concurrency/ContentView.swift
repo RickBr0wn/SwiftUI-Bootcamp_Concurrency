11680e240ee1860981f1ce88b9812ad12209fe47
Rick Brown <ricky.brown.00@gmail.com>
2022-07-04 09:40:56 +0100

    Initial Commit

---
+++ SwiftUI-Bootcamp_Concurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Bootcamp_Concurrency
+//
+//  Created by Rick Brown on 04/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Bootcamp_Concurrency/SwiftUI_Bootcamp_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bootcamp_ConcurrencyApp.swift
+//  SwiftUI-Bootcamp_Concurrency
+//
+//  Created by Rick Brown on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bootcamp_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
